44e892b65f9abf2a152e465a6affba6af3aabb91
simov <simeonvelichkov@gmail.com>
2016-12-22 10:33:59 +0200

    Export whois methods + add slack attachment json

--- bin/whois.js
+++ bin/whois.js
@@ -7,7 +7,7 @@ if (args.help) {
   console.log(render(['Flag', 'Description'], [
     {'--help': 'Help message about `varnalab-whois`'},
     {'--config [file]': 'Specify config file - Required'},
-    {'--output [json|output|clean]': 'JSON / Slack attachment output / ' +
+    {'--output [json|slack|clean]': 'JSON / Slack attachment output / ' +
       'Clean output without UTF8 tables'},
     {'--timeout 5000': 'Timeout in milliseconds - defaults to 5000'}
   ]))
@@ -19,42 +19,45 @@ if (!args.config) {
   process.exit()
 }
 
-var MikroNode = require('mikronode')
-var connection = MikroNode.getConnection(
-  args.config.host, args.config.user, args.config.pass)
 var async = require('async')
+var MikroNode = require('mikronode')
 
+exports.start = () => {
+  var connection = MikroNode.getConnection(
+    args.config.host, args.config.user, args.config.pass)
 
-var chan
-var router = {
-  ondata: (done) => (router.done = done)
+  var chan
+  var router = {
+    ondata: (done) => (router.done = done)
+  }
+
+  async.series({
+    connection: (done) => {
+      connection.connect((conn) => {
+        chan = conn.openChannel()
+        chan.on('done', (body) => router.done(null, MikroNode.parseItems(body)))
+        chan.once('trap', (trap, chan) => router.done(trap))
+        chan.once('error', (err, chan) => router.done(err))
+        done(null, conn)
+      })
+    },
+    leases: (done) => {
+      chan.write('/ip/dhcp-server/lease/getall')
+      router.ondata(done)
+    },
+    active: (done) => {
+      chan.write('/ip/arp/getall')
+      router.ondata(done)
+    }
+  }, (err, result) => {
+    clearTimeout(timeout)
+    var output = exports.print(err, result)
+    console[output instanceof Error ? 'error' : 'log'](output)
+    process.exit()
+  })
 }
 
-async.series({
-  connection: (done) => {
-    connection.connect((conn) => {
-      chan = conn.openChannel()
-      chan.on('done', (body) => router.done(null, MikroNode.parseItems(body)))
-      chan.once('trap', (trap, chan) => router.done(trap))
-      chan.once('error', (err, chan) => router.done(err))
-      done(null, conn)
-    })
-  },
-  leases: (done) => {
-    chan.write('/ip/dhcp-server/lease/getall')
-    router.ondata(done)
-  },
-  active: (done) => {
-    chan.write('/ip/arp/getall')
-    router.ondata(done)
-  }
-}, (err, result) => {
-  clearTimeout(timeout)
-  print(err, result)
-  process.exit()
-})
-
-function print (err, result) {
+exports.print = (err, result) => {
   var timestamp = Math.floor(Date.now() / 1000)
   var error, active
 
@@ -74,18 +77,31 @@ function print (err, result) {
   }
 
   if (args.output === 'json') {
-    console.log(JSON.stringify({timestamp, error, active}))
+    return JSON.stringify({timestamp, error, active})
   }
   else if (args.output === 'slack') {
-    console.log(JSON.stringify({timestamp, error: 'Not implemented!'}))
+    return JSON.stringify({
+      attachments: [{
+        fallback: 'Whois in VarnaLab',
+        text: '💻 *`' + active.length + '`*\n' +
+          active.map((active) =>
+            '_' + active.ip + '_ / *' + active.host + '*'
+          ).join('\n'),
+        ts: timestamp,
+        mrkdwn_in: ['text']
+      }]
+    })
   }
   else {
-    error
-      ? console.error('Error:', error)
-      : console.log(render(['mac', 'ip', 'host'],
-          active.map((lease) => ([lease.mac, lease.ip, lease.host]))
-        ))
+    return error
+      ? new Error('Error: ' + error)
+      : render(['mac', 'ip', 'host'],
+          active.map((lease) => ([lease.mac, lease.ip, lease.host])))
   }
+}
+
+if (!module.parent) {
+  exports.start()
 }
 
 var timeout = setTimeout(() => {
